220ee203a53cb6dd0e16c3a65f0ec358a7004f96
Richard Schneeman <rschneeman@salesforce.com>
2021-09-08 09:32:36 -0500

    Extract into function.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use libcnb::data::build_plan::BuildPlan;
-use libcnb::data::launch::{Launch, Process, ProcessTypeError};
+use libcnb::data::launch::{Launch, Process};
+use std::path::PathBuf;
 
-use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::{YamlLoader};
 
 use libcnb::{
     cnb_runtime, DetectOutcome, GenericBuildContext, GenericDetectContext, GenericErrorHandler,
@@ -18,34 +19,10 @@ fn detect(_context: GenericDetectContext) -> Result<DetectOutcome, std::io::Erro
 }
 
 fn build(context: GenericBuildContext) -> Result<(), std::io::Error> {
-	// println("[INFO] Discovering process types")
-    let procfile = context.app_dir.join("Procfile");
-    let mut launch_vec = vec![];
-
     println!("===============================");
-
-    if procfile.as_path().exists() {
-        let procfile_path = procfile.to_str().unwrap();
-        
-        let procfile_contents = std::fs::read_to_string(procfile_path).unwrap();
-        let contents = YamlLoader::load_from_str(
-            &procfile_contents
-        ).unwrap();
-
-        // println!("{:?}", contents[0]);
-
-        let first = contents[0].as_hash().unwrap();
-        for (key, value) in &*first{
-            let p= Process::new(
-                key.as_str().unwrap(),
-                value.as_str().unwrap(),
-                Vec::<String>::new(),
-                false,
-            )?;
-            launch_vec.push(Launch::default().process(p));
-        }
-    } else {
-    }
+    let launch_vec = launch_vec_from_procfile(
+        context.app_dir.join("Procfile")
+    )?;
 
     for launch in launch_vec {
         context
@@ -53,3 +30,48 @@ fn build(context: GenericBuildContext) -> Result<(), std::io::Error> {
     }
     Ok(())
 }
+
+fn launch_vec_from_procfile(procfile: PathBuf) -> Result<Vec<libcnb::data::launch::Launch>, std::io::Error> {
+    let mut launch_vec = vec![];
+    let procfile_path = procfile.to_str().unwrap();
+    
+    let procfile_contents = std::fs::read_to_string(procfile_path).unwrap();
+    let contents = YamlLoader::load_from_str(
+        &procfile_contents
+    ).unwrap();
+
+    for (key, value) in &*contents[0].as_hash().unwrap(){
+        let p= Process::new(
+            key.as_str().unwrap(),
+            value.as_str().unwrap(),
+            Vec::<String>::new(),
+            false,
+        )?;
+        launch_vec.push(Launch::default().process(p));
+    }
+
+    Ok(launch_vec)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::path::PathBuf;
+
+    fn procfile_fixture_path(file_name: &str) -> PathBuf {
+        let path = env::current_dir().unwrap();
+        path.join("../test/fixtures").join(file_name).join("Procfile")
+    }
+
+    #[test]
+    fn test_lol() {
+        let launch_vec = super::launch_vec_from_procfile(
+            procfile_fixture_path("app_with_procfile")
+        ).unwrap();
+
+        assert_eq!(2, launch_vec.len());
+    }
+}
+
+
