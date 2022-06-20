c4ee682496c8b85ce28f5498aeac913cb8f30001
Ed Morley <501702+edmorley@users.noreply.github.com>
2022-06-20 13:32:30 +0100

    Remove redundant `.buildpacks(vec![BuildpackReference::Crate])`
    Since testing the current buildpack is the default, so no need
    to specify it explicitly.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,7 +8,7 @@
 
 use indoc::indoc;
 use libcnb_test::assert_contains;
-use libcnb_test::{BuildpackReference, IntegrationTest};
+use libcnb_test::IntegrationTest;
 
 #[test]
 #[ignore = "integration test"]
@@ -17,7 +17,6 @@ fn test_web_and_worker_procfile() {
         "heroku/builder:22",
         "tests/fixtures/web_and_worker_procfile",
     )
-    .buildpacks(vec![BuildpackReference::Crate])
     .run_test(|context| {
         assert_contains!(
             context.pack_stdout,
@@ -49,9 +48,8 @@ fn test_web_and_worker_procfile() {
 #[test]
 #[ignore = "integration test"]
 fn test_worker_only_procfile() {
-    IntegrationTest::new("heroku/builder:22", "tests/fixtures/worker_only_procfile")
-        .buildpacks(vec![BuildpackReference::Crate])
-        .run_test(|context| {
+    IntegrationTest::new("heroku/builder:22", "tests/fixtures/worker_only_procfile").run_test(
+        |context| {
             assert_contains!(
                 context.pack_stdout,
                 indoc! {"
@@ -69,7 +67,8 @@ fn test_worker_only_procfile() {
                     let log_output = container.logs_wait();
                     assert_eq!(log_output.stdout, "this is the worker process!\n");
                 });
-        });
+        },
+    );
 }
 
 #[test]
@@ -79,7 +78,6 @@ fn test_multiple_non_web_procfile() {
         "heroku/builder:22",
         "tests/fixtures/multiple_non_web_procfile",
     )
-    .buildpacks(vec![BuildpackReference::Crate])
     .run_test(|context| {
         assert_contains!(
             context.pack_stdout,
@@ -123,9 +121,8 @@ fn test_multiple_non_web_procfile() {
 // Tests a Procfile that happens to not be valid YAML, but is still valid according
 // to the supported Procfile syntax.
 fn test_not_yaml_procfile() {
-    IntegrationTest::new("heroku/builder:22", "tests/fixtures/not_yaml_procfile")
-        .buildpacks(vec![BuildpackReference::Crate])
-        .run_test(|context| {
+    IntegrationTest::new("heroku/builder:22", "tests/fixtures/not_yaml_procfile").run_test(
+        |context| {
             assert_contains!(
                 context.pack_stdout,
                 indoc! {"
@@ -140,15 +137,15 @@ fn test_not_yaml_procfile() {
                     let log_output = container.logs_wait();
                     assert_eq!(log_output.stdout, "foo: bar\n");
                 });
-        });
+        },
+    );
 }
 
 #[test]
 #[ignore = "integration test"]
 fn test_empty_procfile() {
-    IntegrationTest::new("heroku/builder:22", "tests/fixtures/empty_procfile")
-        .buildpacks(vec![BuildpackReference::Crate])
-        .run_test(|context| {
+    IntegrationTest::new("heroku/builder:22", "tests/fixtures/empty_procfile").run_test(
+        |context| {
             assert_contains!(
                 context.pack_stdout,
                 indoc! {"
@@ -157,14 +154,13 @@ fn test_empty_procfile() {
                 "}
             );
             assert_contains!(context.pack_stdout, "no default process type");
-        });
+        },
+    );
 }
 
 #[test]
 #[ignore = "integration test"]
 #[should_panic(expected = "ERROR: No buildpack groups passed detection.")]
 fn test_missing_procfile() {
-    IntegrationTest::new("heroku/builder:22", "tests/fixtures/missing_procfile")
-        .buildpacks(vec![BuildpackReference::Crate])
-        .run_test(|_| {});
+    IntegrationTest::new("heroku/builder:22", "tests/fixtures/missing_procfile").run_test(|_| {});
 }
